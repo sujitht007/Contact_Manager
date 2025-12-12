c1da85a16b334510cf34718118c6ed74312b5910
sujitht007 <sujithsujith9498@gmail.com>
2025-12-12 10:01:00 +0530

    contact-manager

--- client/src/API.jsx
+++ client/src/API.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://contact-manager-1-1nsj.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
